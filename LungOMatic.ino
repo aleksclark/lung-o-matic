a20b753e1fcdbd65df3f7a236764aa8f7a7134fe
Aleks Clark <aleks.clark@gmail.com>
2021-04-03 07:43:03 -0500

    wip

--- LungOMatic.ino
+++ LungOMatic.ino
@@ -5,37 +5,151 @@
 #include "pinout.h"
 #include "oxygen_sensor.h"
 #include "debug_console.h"
+#include "BasicStepperDriver.h"
+#include "A4988.h"
+#include "MultiDriver.h"
+#include "SyncDriver.h"
 
+// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+#define MOTOR_STEPS 200
+// Target RPM for X axis motor
+#define MOTOR_RPM 3000
+// Target RPM for Y axis motor
+
+#define STOPPER_PIN 19
+// 1 motor
+#define DIR_Z 48
+#define STEP_Z 46
+#define SLEEP_Z 62
+// 2 motor
+#define DIR_Y 61
+#define STEP_Y 60
+#define SLEEP_Y 56
+#define MICROSTEPS 16
+// 100mm tube = 314 cm2
+#define CYL_AREA 314
+#define TIDAL_VOLUME 628
+
+#define STEP_DIV 22
+
+// 2-wire basic config, microstepping is hardwired on the driver
+// Other drivers can be mixed and matched but must be configured individually
+A4988 stepperZ(MOTOR_STEPS, DIR_Z, STEP_Z);
+A4988 stepperY(MOTOR_STEPS, DIR_Y, STEP_Y);
+bool isHome = false;
+bool insp = false;
+SyncDriver controller(stepperZ, stepperY);
 
 void setup() {
   Serial.begin(115200);
 
 
   // Setup O2 sensor
+
   setup_o2sensor();
+  // stepperZ.begin(MOTOR_RPM, MICROSTEPS);
+  // stepperY.begin(MOTOR_RPM, MICROSTEPS);
+
+  pinMode(62, OUTPUT);
+  pinMode(56, OUTPUT);
+  digitalWrite(62, LOW);
+  digitalWrite(56, LOW);
+
+  pinMode(STOPPER_PIN, INPUT_PULLUP);
+  stepperZ.begin(MOTOR_RPM, MICROSTEPS);
+  stepperY.begin(MOTOR_RPM, MICROSTEPS);
+  stepperY.enable();
+  stepperZ.enable();
+
+
+  home();
+
+
 }
 
-
-void delay_with_dbg_info(int milliseconds)
-{
-  DBG_println_float("- Sleeping ", (float)milliseconds/1000, " seconds ...", 3);
-  delay(milliseconds);
+void move(int mm) {
+  long quant = 100 * MOTOR_STEPS * MICROSTEPS / STEP_DIV;
+  quant = quant * mm;
+  controller.startMove(quant, quant);
+  
 }
 
+void home() {
 
-void halfCycle(int state_5way)
-{
-  // Read O2 value
-  loop_o2sensor(100);
+  move(-350);
+  while(!isHome){
+      // statement
+      if (digitalRead(STOPPER_PIN) == LOW){
+            Serial.println("STOPPER REACHED");
 
-  delay_with_dbg_info(1000);
+            /*
+             * Choosing stop() vs startBrake():
+             *
+             * constant speed mode, they are the same (stop immediately)
+             * linear (accelerated) mode with brake, the motor will go past the stopper a bit
+             */
+
+            stepperY.stop();
+            stepperZ.stop();
+            // stepperY.disable();
+            // stepperZ.disable();
+            // digitalWrite(62, HIGH);
+            // digitalWrite(56, HIGH);
+            move(10);
+            isHome = true;
+            // stepper.startBrake();
+        }
+      controller.nextAction();
+  }
+
+  bool moving = true;
+  while(moving) {
+    unsigned wait_time_micros = controller.nextAction();
+    if (wait_time_micros <= 0) {
+      moving = false;
+    }
+  }
+
+}
+
+// void halfCycle(int state_5way)
+// {
+//   // Read O2 value
+//   // loop_o2sensor(100);
+
+//   delay_with_dbg_info(1000);
   
 
-}
+// }
 
 void loop() {
-  DBG_println_buffered("- Start HIGH cycle ...")
-  halfCycle(HIGH);
-  DBG_println_buffered("- Start LOW cycle ...")
-  halfCycle(LOW);  
+  if (digitalRead(STOPPER_PIN) == LOW){
+      Serial.println("STOPPER REACHED");
+
+      /*
+       * Choosing stop() vs startBrake():
+       *
+       * constant speed mode, they are the same (stop immediately)
+       * linear (accelerated) mode with brake, the motor will go past the stopper a bit
+       */
+
+      stepperY.stop();
+      stepperZ.stop();
+      stepperY.disable();
+      stepperZ.disable();
+      digitalWrite(62, HIGH);
+      digitalWrite(56, HIGH);
+      // stepper.startBrake();
+  }
+
+  unsigned wait_time_micros = controller.nextAction();
+  if (wait_time_micros <= 0) {
+    if (insp) {
+      insp = false;
+      move(-(TIDAL_VOLUME / CYL_AREA) * 10);
+    } else {
+      insp = true;
+      move((TIDAL_VOLUME / CYL_AREA) * 10);
+    }
+  }
 }
--- oxygen_sensor.cpp
+++ oxygen_sensor.cpp
@@ -48,7 +48,10 @@ void setup_o2sensor()
     #ifdef O2SENSE_NEED_METADATA
   
     // I don't think these commands are working
-  
+    for (int i = 0; i < cmd_vernum[1] + 3; i++)
+          {
+            DBG_print_buffered("%02X ", cmd_vernum[i]);
+          }
     softSer.write((uint8_t*)cmd_vernum, 4);
     delay(500);
     softSer.write((uint8_t*)cmd_sernum, 4);
@@ -64,83 +67,31 @@ void setup_o2sensor()
 // Print sensor values. Constrain execution time in the given number of milliseconds 
 void loop_o2sensor(int wait_after_ms)
 {
+
+  const uint8_t cmd_read[] = {O2SENSE_CMD_READ_DATA};
   unsigned long time_start_ms = time_start_ms = millis();
-  
+  softSer.write((uint8_t*)cmd_read, 4);
+  delay(20);
   for(int i = 0; i < bytes_per_packet; i++)
   {
     if (softSer.available()) // at least 1 byte from UART arrived
     {
-      DBG_println("got a byte");
+
       o2sens_feedUartByte(softSer.read()); // give byte to the parser
-      uint8_t* all_data = o2sens_getRawBuffer();
-      for (int i = 0; i < all_data[1] + 3; i++)
-          {
-            DBG_print_buffered("%02X ", all_data[i]);
-          }
       if (o2sens_hasNewData()) // a complete packet has been processed
       {
-        DBG_println("new_data");
+
         o2sens_clearNewData(); // clear the new packet flag
-        DBG_println_buffered(", %u , %u , %u"
-            , o2sens_getConcentration16()
-            , o2sens_getFlowRate16()
-            , o2sens_getTemperature16()
-            );
-        // print out the whole buffer for debugging
 
+        float o2concentration = (float)o2sens_getConcentration16()/10;
+        float o2flow = (float)o2sens_getFlowRate16()/10;
+        float ambientTemperature = o2sens_getTemperature16()/10;
 
-        // if (sensor_log_enabled)
-        // {
-        //   // display time in milliseconds for logging
-        //   DBG_print(millis(), DEC); 
-        //   DBG_print(": ");
-    
-          
-
-        //   // print out the human readable data
-        //   DBG_println_buffered(", %u , %u , %u"
-        //     , o2sens_getConcentration16()
-        //     , o2sens_getFlowRate16()
-        //     , o2sens_getTemperature16()
-        //     );
-
-        //   if (has_sernum == false)
-        //   {
-        //     uint8_t* data = o2sens_getSerialNumber();
-        //     if (data[0] != 0 || data[1] != 0 || data[2] != 0 || data[3] != 0 || data[4] != 0)
-        //     {
-        //       has_sernum = true;
-        //       DBG_println_buffered("Serial Number: %02X %02X %02X %02X %02X"
-        //         , data[0]
-        //         , data[1]
-        //         , data[2]
-        //         , data[3]
-        //         , data[4]
-        //         );
-        //     }
-        //   }
-        //   if (has_vernum == false)
-        //   {
-        //     char* data = o2sens_getVersionNumber();
-        //     if (data[0] != 0) // first byte 
-        //     {
-        //       has_vernum = true;
-        //       DBG_println_buffered("Version Number: %s", data);
-        //     }
-        //   }
-        // }
-
-        // float o2concentration = (float)o2sens_getConcentration16()/10;
-        // float o2flow = (float)o2sens_getFlowRate16()/10;
-        // float ambientTemperature = o2sens_getTemperature16()/10;
-
-        // DBG_println("- Sensor data:");
-        // DBG_println_float("     O2 concentration = ", o2concentration, "%", 1);
-        // DBG_println_float("     Flow = ", o2flow, " liter/min", 1);
-        // DBG_println_float("     Temperature = ", ambientTemperature, " Celsius", 1);
+        DBG_println("- Sensor data:");
+        DBG_println_float("     O2 concentration = ", o2concentration, "%", 1);
+        DBG_println_float("     Flow = ", o2flow, " liter/min", 1);
+        DBG_println_float("     Temperature = ", ambientTemperature, " Celsius", 1);
       }
-    } else {
-      DBG_print("no byte");
     }
   }
 
@@ -151,6 +102,5 @@ void loop_o2sensor(int wait_after_ms)
     DBG_println_buffered("- Time spent in loop_sensor() = %d milliseconds", time_delta_ms);
   }
 
-  int time_left_to_wait_ms = wait_after_ms - time_delta_ms;
-  delay(time_left_to_wait_ms);
+
 }
